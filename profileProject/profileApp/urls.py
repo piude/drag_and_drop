73348a2a186d178988114593b488be70c585a6f8
Piu De <piuwebkrone@gmail.com>
2022-12-13 18:22:42 +0530

    Profile creation using signal implemented

--- profileProject/profileApp/urls.py
+++ profileProject/profileApp/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from profileApp import views
 
 app_name='profileApp'
-urlpatterns = []
+
+urlpatterns = [
+		path('signup/', views.signup, name='signup'),
+		path('profile_list/', views.profile_list, name='profile_list'),
+		path('profile_detail/<id>', views.profile_detail, name='profile_detail'),
+		path('edit_profile/<id>' ,views.edit_profile, name='edit_profile'),
+]
--- profileProject/profileApp/views.py
+++ profileProject/profileApp/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from .models import Profile
+from .forms import ProfileForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
 # Create your views here.
+
+def signup(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account created successfully for user {username}')
+			return redirect('profileApp:profile_list')
+	else:
+		form = UserCreationForm()
+	return render(request, 'signup.html', {'form':form})
+
+
+def profile_list(request):
+	context = {}
+	context['profiles'] = Profile.objects.all()
+	return render(request, 'profile_list.html', context)
+
+def profile_detail(request, id):
+	context = {}
+	context['profile'] = Profile.objects.get(id=id)
+	return render(request, 'profile_detail.html', context)
+
+def edit_profile(request, id):
+	context = {}
+	obj = get_object_or_404(Profile, id=id)
+	form = ProfileForm(request.POST or None, instance = obj)
+	if form.is_valid():
+		form.save()
+		return redirect('profileApp:profile_list')
+	context['form'] = form
+	return render(request, 'edit_profile.html', context)
